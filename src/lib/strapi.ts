38a092827c706adad74e7f5b171f47b53a94b6cf
Pitoune <pierredheb@gmail.com>
2024-01-09 10:47:26 +0100

    git the API key like a boss

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -23,7 +23,7 @@ export default async function fetchApi<T>({
     endpoint = endpoint.slice(1);
   }
 
-  const url = new URL(`${import.meta.env.STRAPI_URL}/api/${endpoint}`);
+  const url = new URL(`https://api.genealogie.dhebrail.fr/api/${endpoint}`);
 
   if (query) {
     Object.entries(query).forEach(([key, value]) => {
@@ -32,7 +32,7 @@ export default async function fetchApi<T>({
   }
   const res = await fetch(url.toString(), {
     headers: {
-      Authorization: `Bearer ${import.meta.env.STRAPI_TOKEN}`,
+      Authorization: `Bearer f168d6ea72222718eb222dba3720aa7b06d529a8fac41e804b952c24c5c405183de4054bc4c057501a3aacb0e482882f340eaed5d30d6765e626b1bcd084ab9a44ca68419f6fbef19d7d711d4ab6b4e2014548592943473d5642a89798a614b1ada0d17bc7ac5cb6c13ae66b4bc2e20746759c1035d61e6e266eec36e3fab485`,
     },
   });
   let data = await res.json();
