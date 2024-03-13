fd3b3b660981a7b3619eda4d9f1e6c43b53d8ea9
Pitoune <86973986+DarkPitoune@users.noreply.github.com>
2024-03-13 11:54:36 +0100

    Update readme.sh

--- server/readme.sh
+++ server/readme.sh
@@ -1,7 +1,5 @@
 if [ -f "pocketbase" ]; then
   ./pocketbase
-  # Replace this line with the command you want to execute if the file exists
 else
   echo "Install pocketbase first: https://pocketbase.io"
-  # Replace this line with the command you want to execute if the file does not exist
 fi
