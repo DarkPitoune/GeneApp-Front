db04d2b6da3808f32b1a142c45fb7d0369edb083
Pitoune <pierredheb@gmail.com>
2024-03-30 11:45:01 +0100

    htmx from server, not cdn

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
+import htmx from "astro-htmx";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), htmx()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
